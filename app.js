e265fbc3d2e3cb5ae6a692d6686c3ef547dfc74c
Paul Cummins <prcummins@gmail.com>
2026-03-18 17:31:08 +0000

    Add delete button on draft bracket cards

--- app.js
+++ app.js
@@ -464,7 +464,10 @@ function renderMyBracketsPage() {
                 </div>
                 ${b.submitted
                   ? `<span class="bracket-card-badge submitted">Submitted</span>`
-                  : `<span class="bracket-card-badge draft">Draft</span>`
+                  : `<span class="bracket-card-badge draft">Draft</span>
+                     <button class="bracket-card-delete" onclick="event.stopPropagation(); deleteBracket(${b.id})" title="Delete draft">
+                       <svg viewBox="0 0 20 20" fill="currentColor" width="14" height="14"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"/></svg>
+                     </button>`
                 }
               </div>
               <div class="bracket-card-stats">
